5397e976ec575eac7b1a278722b8fbdb9995d1aa
Sean Keys <skeys@google.com>
2018-01-21 16:02:40 -0700

    Initial "hello world" commit.

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # PowerEFI_Loader
 SmartFusion2 Loader
 
+## Building sources
+From base project directory run: bazel build sf2_cli_loader
+
 ## Pushing Code For Review
 TODO
---
+++ src/cli/main.cc
@@ -0,0 +1,18 @@
+/*
+ * main.cc
+ *
+ *  Created on: Jan 21, 2018
+ *      Author: skeys
+ */
+
+
+#include <iostream>
+
+using std::cout;
+
+int main() {
+
+  cout << "Yo!";
+  return 0;
+}
+
