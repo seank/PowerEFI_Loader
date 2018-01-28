5ebb17414972b0e10f4925aa9607cccdc4b19c8d
Sean Keys <skeys@ipdatasys.com>
2018-01-28 00:59:15 -0700

    Print errno details.
    TESTS=made a bad fopen call.
    
    Change-Id: I29c11012667ef734626d7ea61fa148f9b6ac0251

--- src/lib/logging/logger.cc
+++ src/lib/logging/logger.cc
@@ -7,10 +7,14 @@
 
 #include "log-impl.h"
 
+#include <cstring>
+
 using namespace logging;
 
 void file_log_policy::open_ostream(const std::string& name) {
   out_stream->open(name.c_str(), std::ios_base::binary | std::ios_base::out);
+  /* TODO: needs to be tested on win/droid */
+  freopen (name.c_str(),"w",stderr);
   if (!out_stream->is_open()) {
     throw(std::runtime_error("LOGGER: Unable to open an output stream"));
   }
@@ -23,8 +27,17 @@ void file_log_policy::close_ostream() {
 }
 
 void file_log_policy::write(const std::string& msg) {
-  (*out_stream) << msg << std::endl;
-  std::cout << msg << std::endl;
+  std::string log_msg = msg;
+  // Add system call error details.
+  if (errno != 0) {
+    std::string sys_msg = strerror(errno);
+    errno = 0;
+    log_msg += " Error info: ";
+    log_msg += sys_msg;
+  }
+
+  (*out_stream) << log_msg << std::endl;
+    std::cout << log_msg << std::endl;
 }
 
 file_log_policy::~file_log_policy() {
